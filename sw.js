90e7abb81dda3d9a5f47d5a4bc17e3e1c4391c51
hotistudio <justduducha@gmail.com>
2026-03-26 17:51:02 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// wnstudio sw.js - updated: 1774513928
-const CACHE = 'wnstudio-1774513928';
+// wnstudio sw.js - updated: 1774514919
+const CACHE = 'wnstudio-1774514919';
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/wnstudio/', '/wnstudio/index.html']))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => { if(e.request.method!=='GET') return; e.respondWith(fetch(e.request,{cache:'no-cache'}).then(res => { if(res&&res.status===200){const clone=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,clone));} return res; }).catch(()=>caches.match(e.request))); });
-self
+self.addEventListener('message', e => { if(e.data?.type==='SKIP_WAITING') self.skipWaiting(); });
