75fa485f88c2fc9efc751b37a4e9520c024583e5
hotistudio <justduducha@gmail.com>
2026-03-26 15:40:07 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// wnstudio sw.js - updated: 1774505345
-const CACHE = 'wnstudio-1774505345';
+// wnstudio sw.js - updated: 1774507184
+const CACHE = 'wnstudio-1774507184';
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/wnstudio/', '/wnstudio/index.html'])));
