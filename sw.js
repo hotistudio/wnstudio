fbe19401e54d81809475f304c5740e33682b775e
hotistudio <justduducha@gmail.com>
2026-03-26 16:21:52 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,6 @@
-// wnstudio sw.js - updated: 1774507679
-const CACHE = 'wnstudio-1774507679';
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/wnstudio/', '/wnstudio/index.html'])));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(fetch(e.request, {cache:'no-cache'}).then(res => {
-    if (res && res.status === 200) {
-      const clone = res.clone();
-      caches.open(CACHE).then(cache => cache.put(e.request, clone));
-    }
-    return res;
-  }).catch(() => caches.match(e.request)));
-});
-self.addEventListener('message', e => {
-  if (e.data?.type === 'SKIP_WAITING') self.skipWaiting();
-});
+// wnstudio sw.js - updated: 1774509602
+const CACHE = 'wnstudio-1774509602';
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/wnstudio/', '/wnstudio/index.html']))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { if(e.request.method!=='GET') return; e.respondWith(fetch(e.request,{cache:'no-cache'}).then(res => { if(res&&res.status===200){const clone=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,clone));} return res; }).catch(()=>caches.match(e.request))); });
+self.addEventListener('message', e => { if(e.data?.type==='SKIP_WAITING') self.skipWaiting(); });
